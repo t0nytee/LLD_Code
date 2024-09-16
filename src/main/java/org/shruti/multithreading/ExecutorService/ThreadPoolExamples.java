1760601343c60b32104f8759ab4ee0cc3d18f086
Shruti Rani <shruti.rani@salesforce.com>
2024-09-16 20:14:36 +0530

    ScheduledExecutorService

--- src/main/java/org/shruti/multithreading/ExecutorService/ThreadPoolExamples.java
+++ src/main/java/org/shruti/multithreading/ExecutorService/ThreadPoolExamples.java
@@ -2,6 +2,7 @@ package org.shruti.multithreading.ExecutorService;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolExamples {
 
@@ -23,6 +24,23 @@ public class ThreadPoolExamples {
         singleThreadExecutor.submit(() -> {
             System.out.println("Hello single thread pool");
         });
+
+        singleThreadExecutor.shutdown();
+// Threadpool executor will stop accepting new task in submission queue, but will continue to process existing tasks
+
+        try{
+            // calling thread(main thread) will wait for 2 seconds after calling the shutdown
+            // called after executor.shutdown()
+            boolean isShutdown = singleThreadExecutor.awaitTermination(2, TimeUnit.SECONDS);
+        } catch (Exception e) {
+
+        }
+
+        fixedThreadPool.shutdownNow();
+        // shutdownnow => stops/interrupts the actively executing task
+        //halts the waiting tasks
+        // returns list of task awaiting execution
+
 
 
     }
